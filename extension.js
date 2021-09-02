cfe5c8c88f075c72cd0144913863018995a52ffd
freeton20 <doctor4891@gmail.com>
2021-09-02 22:47:44 +0300

    hover

--- extension.js
+++ extension.js
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const snippets_json = require("./snippets/ton-solidity.json")
+const wordsSet = snippets_json['.source.ton-solidity'];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,18 +15,25 @@ function activate(context) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ton-solidity" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ton-solidity.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ton-solidity!');
+	disposable = vscode.languages.registerHoverProvider('ton-solidity', {
+		provideHover(document, position) {
+			const wordRange = document.getWordRangeAtPosition(position,new RegExp(/[A-Za-z\.]+/));
+			const word = document.getText(wordRange);
+			let suggestion = null;
+			for(const [, value] of Object.entries(wordsSet)){
+				if(value.prefix == word)
+				if(Array.isArray(value.description)){					
+					suggestion = value.description.join("\n")
+				}else{
+					suggestion = value.description;
+				}   
+			}				
+			return new vscode.Hover(suggestion);
+		  }
 	});
-
+	
 	context.subscriptions.push(disposable);
+
 }
 
 // this method is called when your extension is deactivated

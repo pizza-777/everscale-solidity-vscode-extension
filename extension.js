4a6b875a497039f9381616359eb226e2e02c2384
freeton20 <doctor4891@gmail.com>
2021-09-04 15:19:10 +0300

    commit

--- extension.js
+++ extension.js
@@ -1,17 +1,14 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const { controllers } = require('tondev');
 const snippets_json = require("./snippets/ton-solidity.json")
 const wordsSet = snippets_json['.source.ton-solidity'];
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-
+	console.log(controllers);
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "ton-solidity" is now active!');
@@ -34,6 +31,42 @@ function activate(context) {
 
 	context.subscriptions.push(disposable);
 
+	const collection = vscode.languages.createDiagnosticCollection("tonsol");
+
+	if(vscode.window.activeTextEditor){
+		updateDiagnostics(vscode.window.activeTextEditor.document, collection)
+	}
+	context.subscriptions.push(vscode.window.onDidChangeActiveTextEditor(editor => {
+		console.log('ondidchangetxteditor');
+		if (editor) {
+			updateDiagnostics(editor.document, collection);
+		}
+	}));
+	context.subscriptions.push(vscode.workspace.onDidChangeTextDocument(documentChangeEvent => {
+		 console.log('ondidchangetxtdocument');
+		if (documentChangeEvent) {
+			updateDiagnostics(documentChangeEvent.document, collection);
+		}
+	}));
+
+}
+
+function updateDiagnostics(document, collection) {
+	console.log(document.uri.fsPath);
+	//if (document && path.basename(document.uri.fsPath) === 'sink.sol') {
+		collection.set(document.uri, [{
+			code: '',
+			message: 'cannot assign twice to immutable variable `x`',
+			range: new vscode.Range(new vscode.Position(3, 4), new vscode.Position(3, 10)),
+			severity: vscode.DiagnosticSeverity.Error,
+			source: '',
+			relatedInformation: [
+				new vscode.DiagnosticRelatedInformation(new vscode.Location(document.uri, new vscode.Range(new vscode.Position(1, 8), new vscode.Position(1, 9))), 'first assignment to `x`')
+			]
+		}]);
+	//} else {
+	//	collection.clear();
+	//}
 }
 
 // this method is called when your extension is deactivated

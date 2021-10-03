8014a39cee3bee006970782b209ab43f91f9fcdc
freeton20 <doctor4891@gmail.com>
2021-10-03 23:10:04 +0300

    update

--- test/contracts/common.sol
+++ test/contracts/common.sol
@@ -12,9 +12,13 @@ contract Test {
 
     enum testEnum {item1, item2}
 
-    event name(address addr );
+    event name(address addr);
 
     mapping (uint=>string) testMapping;
+
+    struct Name {
+        uint b;    
+    }    
 
     constructor(uint a) public {}
 
@@ -31,10 +35,74 @@ contract Test {
     }
    
    function fview(uint b) view public returns (uint) {
+       repeat(10) {
+           
+       }
+       for ((uint256 key, string value) : testMapping) { // iteration over mapping 
+          
+       }      
+       bytes byteArray = "Hello!";
+       for (byte d : byteArray) {
+            
+       }
+
        return b;
    }
-   
-   function tonUnits() public returns (public name) {
-       
+
+   function fret(uint c) internal returns (uint) {
+       return c;
    }
+    TvmCell tcellvar;
+    uint32 key;
+    uint256 value;
+    function tvmcellfunc() public {
+        tcellvar.depth();       
+        tcellvar.dataSize(10);
+        tcellvar.dataSizeQ(1);
+        tcellvar.toSlice();
+    }
+
+    function optTest() public {
+        optional(uint) opt;
+        opt.set(11);
+        opt = 22; 
+        opt.get() = 33;
+        opt.reset();
+        optional(uint) x = 123;  
+    }
+
+    function vectorTest() public {
+        vector(uint) vect;
+        uint a = 11;
+        vect.push(a);
+        vect.push(111);
+        vect.pop();
+        vect.length();
+        vect.empty();        
+    }
+
+    function tonUnits() public {
+        require(1 nano == 1);
+        require(1 nanoton == 1);
+        require(1 nTon == 1);
+        require(1 ton == 1e9 nanoton);
+        require(1 Ton == 1e9 nanoton);
+        require(1 micro == 1e-6 ton);
+        require(1 microton == 1e-6 ton);
+        require(1 milli == 1e-3 ton);
+        require(1 milliton == 1e-3 ton);
+        require(1 kiloton == 1e3 ton);
+        require(1 kTon == 1e3 ton);
+        require(1 megaton == 1e6 ton);
+        require(1 MTon == 1e6 ton);
+        require(1 gigaton == 1e9 ton);
+        require(1 GTon == 1e9 ton);
+    }
+
+     function dst(TvmCell message, uint n) public {
+         message.depth();
+         message.dataSize(n);
+         message.     
+    }
+    
 }

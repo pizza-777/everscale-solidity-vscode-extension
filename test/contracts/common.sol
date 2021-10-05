61990e532b3c18d2bc767f4009b04e0e36c61e9e
freeton20 <doctor4891@gmail.com>
2021-10-05 10:35:06 +0300

    tests sol

--- test/contracts/common.sol
+++ test/contracts/common.sol
@@ -3,10 +3,21 @@ pragma AbiHeader time;
 pragma AbiHeader pubkey;
 pragma AbiHeader expire;
 pragma ignoreIntOverflow;
-pragma msgValue 1;
+pragma msgValue 1e10;
 //import './import.sol';
 
-library TestLib {}
+
+library MathHelper {
+    // State variables are forbidden in library but constants are not
+    uint constant MAX_VALUE = 300;
+
+    // Library function
+    function sum(uint a, uint b) internal pure returns (uint) {
+        uint c = a + b;
+        require(c < MAX_VALUE);
+        return c;
+    }
+}
 interface TestInterface {}
 contract Test {
 
@@ -16,18 +27,26 @@ contract Test {
 
     mapping (uint=>string) testMapping;
 
+    uint static a; // ok
+    uint public static b;
+
     struct Name {
         uint b;    
     }    
 
     constructor(uint a) public {}
 
-    function testFunc(uint b) public {
-         for (uint256 index = 0; index < 100; index++) {}
+    function testFunc1(uint b) public {
+         for (uint256 index = 0; index > 100; index++) {}
 
          if (true) {
          } else {
          }   
+         //Name.unpack();
+        uint[] arr;
+        require(arr.empty());
+        arr.push();
+        require(!arr.empty());
     }
 
     modifier testModifier() {
@@ -68,7 +87,9 @@ contract Test {
         opt = 22; 
         opt.get() = 33;
         opt.reset();
-        optional(uint) x = 123;  
+        optional(uint) x = 123; 
+        opt.hasValue(); 
+        
     }
 
     function vectorTest() public {
@@ -124,5 +145,316 @@ contract Test {
          s.loadSlice(n);
          s.decodeFunctionParams(dst);
          s.skip(n,n);
+
+         TvmBuilder builder;
+         builder.toSlice();
+         builder.toCell();
+         //builder.size();
+         builder.bits();
+         builder.refs();
+         builder.remBits();
+         builder.remRefs();
+         builder.remBitsAndRefs();
+         builder.depth();
+         builder.store(message, n, u16, u8);
+         builder.storeOnes(n);
+         //builder.storeZeroes(0);
+         builder.storeSigned(int(-109), u16);
+         builder.storeUnsigned(u8,u16);
+         builder.storeRef(message);
+         builder.storeTons(uint128(10));
+         
+         ExtraCurrencyCollection curCol;
+          optional(uint32, uint256) res = curCol.min();
+         res = curCol.next(1);
+         res = curCol.prev(1);
+         res = curCol.nextOrEq(1);
+         res = curCol.prevOrEq(1);
+         res = curCol.delMin();
+         res = curCol.delMax();
+         //res = curCol.fetch(k);
+        bool exists = curCol.exists(key);
+        bool isEmpty = curCol.empty();
+        bool success = curCol.replace(key, value);
+        success = curCol.add(key, value);
+        bool index; 
+        //success = curCol[index];
+    }
+
+    function other(string s, bytes b, uint i) public {
+        b.empty();
+        bytes byteArray = "abba";
+        int index = 0;
+        byte a0 = byteArray[i];
+        byteArray = "01234567890123456789";
+        bytes slice = byteArray[5:10];
+        bytes etalon = "56789";
+        require(slice == etalon);
+        slice = byteArray[10:];
+        etalon = "0123456789";
+        require(slice == etalon);
+        slice = byteArray[:10];
+        require(slice == etalon);
+        slice = byteArray[:];
+        require(slice == byteArray);
+        require(byteArray[:10] == etalon);
+        require(etalon == byteArray[:10]);
+        b.dataSize(i);
+        b.dataSizeQ(i);
+        b.append(b);
+        byteArray = "1234";
+        //bytes4 bb = byteArray;
+        s.byteLength();
+        s.substr(i);
+        //s.find(s);
+        //s.findLast(s);
+        string str = format("Hello {} 0x{:X} {}  {}.{} tons", 123, 255, address.makeAddrStd(-33,0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF123456789ABCDE), 100500, 32);
+        require(str == "Hello 123 0xFF -21:7fffffffffffffffffffffffffffffffffffffffffffffffff123456789abcde  100500.32 tons", 101);
+        require(format("Hello {}", 123) == "Hello 123", 102);
+        require(format("Hello 0x{:X}", 123) == "Hello 0x7B", 103);
+        require(format("{}", -123) == "-123", 103);
+        require(format("{}", address.makeAddrStd(127,0)) == "7f:0000000000000000000000000000000000000000000000000000000000000000", 104);
+        require(format("{}", address.makeAddrStd(-128,0)) == "-80:0000000000000000000000000000000000000000000000000000000000000000", 105);
+        require(format("{:6}", 123) == "   123", 106);
+        require(format("{:06}", 123) == "000123", 107);
+        require(format("{:06d}", 123) == "000123", 108);
+        require(format("{:06X}", 123) == "00007B", 109);
+        require(format("{:6x}", 123) == "    7b", 110);
+        uint128 a = 1 ton;
+        require(format("{:t}", a) == "1.000000000", 101);
+        a = 123;
+        require(format("{:t}", a) == "0.000000123", 103);
+        fixed32x3 v = 1.5;
+        require(format("{}", v) == "1.500", 106);
+        fixed256x10 vv = -987123.4567890321;
+        require(format("{}", vv) == "-987123.4567890321", 108);
+
+        uint res;
+        bool status;
+        (res, status) = stoi("123");
+        require(status, 111);
+        require(res == 123, 101);
+        string hexstr = "0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF123456789ABCDE";
+        uint num = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF123456789ABCDE;
+        (res, status) = stoi(hexstr);
+        require(status, 112);
+        require(res == num, 102);
+        (res, status) = stoi("0xag");
+        require(!status, 116);
+
+        uint address_value;
+        address addrStd = address(address_value);
+
+        int8 wid;
+        uint addr;
+        address addrr = address.makeAddrStd(wid, addr);
+
+        address addrNone = address.makeAddrNone();
+
+        uint addrNumber;
+        uint bitCnt;
+        address addrExtern = address.makeAddrExtern(addrNumber, bitCnt);
+
+        //addrr.wid(); 
+
+        address(this).currencies; 
+        address(this).balance; 
+        address(this).getType();
+
+        address dest = address(0);
+        uint128 m = 100000;
+        bool bounce = true;
+        uint16 flag = 2;
+        TvmCell body;
+        ExtraCurrencyCollection c;
+        TvmCell stateInit;
+        // sequential order of parameters
+        dest.transfer(m);
+        dest.transfer(m, bounce);
+        dest.transfer(m, bounce, flag);
+        dest.transfer(m, bounce, flag, body);
+        dest.transfer(m, bounce, flag, body, c); 
+        // using named parameters
+        dest.transfer({value: m, bounce: false, flag: 128, body: body, currencies: c});
+        dest.transfer({bounce: false, value: 1 ton, flag: 128, body: body});
+        dest.transfer({value: 1 ton, bounce: false});
+        
+        testMapping.min();
+        testMapping.max();
+        testMapping.next(1);
+        testMapping.prev(1);
+        testMapping.nextOrEq(1);
+        testMapping.prevOrEq(1);
+        testMapping.fetch(1);
+        testMapping.exists(1);
+        testMapping.empty();
+        testMapping.replace(1,'2');
+        testMapping.add(1,'2');
+        testMapping.getSet(1,'2');
+        testMapping.getAdd(1,'2');
+        testMapping.getReplace(1,'2');
+    }
+
+    function getSum(int a, int b) internal pure returns (int) {
+        return a + b;
+    }
+
+    function getSub(int a, int b) internal pure returns (int) {
+        return a - b;
+    }
+
+    function process(int a, int b, uint8 mode) public returns (int) {
+        function (int, int) returns (int) fun;
+        if (mode == 0) {
+            fun = getSum;
+        } else if (mode == 1) {
+            fun = getSub;
+        }
+        return fun(a, b); // if `fun` isn't initialized then exception is thrown 
+    }
+
+    function re() public {
+        uint a = 5;
+
+        require(a == 5); // ok
+        require(a == 6); // throws an exception with code 100
+        require(a == 6, 101); // throws an exception with code 101
+        require(a == 6, 101, "a is not equal to six"); // throws an exception with code 101 and string
+        require(a == 6, 101, a); // throws an exception with code 101 and number a
+
+        a = 5;
+        revert(); // throw exception 100
+        revert(101); // throw exception 101
+        revert(102, "We have a some problem"); // throw exception 102 and string
+        revert(101, a); // throw exception 101 and number a
+
+        msg.sender;
+        msg.value;
+        msg.currencies;
+        msg.pubkey();
+        msg.isExternal;
+        msg.isInternal;
+        msg.createdAt;
+        msg.data;
+        
+        tvm.accept();
+        tvm.commit();
+        tvm.rawCommit();
+        //tvm.getData();
+        TvmCell c;
+        tvm.setData(c);
+        tvm.log('log');
+        TvmBuilder bld;
+        bld.storeUnsigned(0x9876543210, 40);
+        c = bld.toCell(); 
+        tvm.hexdump(a);
+        tvm.bindump(c);
+        a = 123;
+        tvm.hexdump(a);
+        tvm.bindump(a); 
+        int b = -333;
+        tvm.hexdump(b);
+        tvm.bindump(b);
+
+        tvm.setcode(c);
+        tvm.configParam(a);
+        tvm.rawReserve(1, 2);     
+        tvm.hash('string');
+        tvm.insertPubkey(c, msg.pubkey());
+        tvm.buildStateInit(c,c);
+        tvm.stateInitHash(256, 256, 256, 16);
+        tvm.buildEmptyData(msg.pubkey());
+        TvmCell code = tvm.code();        
+        tvm.codeSalt(code);
+        TvmCell salt;
+        tvm.setCodeSalt(code, salt);
+        tvm.pubkey();
+        tvm.setPubkey(msg.pubkey());
+        tvm.setCurrentCode(code);
+        tvm.resetStorage();
+        tvm.functionId(dst);  
+        tvm.encodeBody(dst,dst, a);
+        tvm.exit();
+        tvm.exit1();  
+        uint8 flag;
+        TvmCell msg;
+        tvm.sendrawmsg(msg, flag); 
+        uint a = 1;
+        uint b = 2;
+        math.min(a, b);
+        math.max(a, b);
+        math.minmax(a, b);
+        math.abs(a);
+        math.modpow2(a, b);
+        math.divc(a, b);
+        math.divr(a, b);
+        math.muldiv(a, b);
+        math.muldivc(a, b);
+        math.muldivmod(a, b);
+        math.divmod(a, b);
+        tx.timestamp;
+        //block.timestamp;
+        rnd.next(10);
+        rnd.getSeed();
+        rnd.setSeed(a);
+        rnd.shuffle();
+
+        uint b = 3;
+        uint32 p = 4;
+        uint res = b ** p;
+        require(res == 81);
+
+        selfdestruct(address(0));
+        TvmSlice s;
+        sha256(s);
+        gasToValue(1000, 0);
+        valueToGas(10000, 0);
+
+        tvm.buildExtMsg({
+            dest: address,
+            time:uint64,
+            expire:uint64,
+            call:{f1},
+            sign:bool,
+            pubkey:optional(uint256),
+            abiVer:uint8,
+            callbackId:uint32,
+            onErrorId:uint32,
+            stateInit:TvmCell,
+            signBoxHandle:optional(uint32)
+            });        
+        tvm.buildIntMsg({
+            dest:address,
+            value:uint128,
+            call:{f1},
+            bounce:bool,
+            currencies:ExtraCurrencyCollection
+            });
     }    
-}
+    
+    fallback() external { 
+      
+    }
+    
+    onBounce(TvmSlice body) external {
+    /*...*/
+    } 
+    
+    onTickTock(bool isTock) external {
+    /*...*/
+    }   
+    function onCodeUpgrade() private{
+    /*...*/
+    }
+
+    function f1() public pure functionID(123) {
+    /*...*/
+    }
+
+    function f2() public externalMsg { // this function receives only external messages 
+    /*...*/
+    }        
+ }
+
+
+ 
---
+++ test/contracts/testDebot.sol
@@ -0,0 +1,152 @@
+pragma ton-solidity >=0.46.0;
+pragma AbiHeader expire;
+pragma AbiHeader time;
+pragma AbiHeader pubkey;
+import "https://raw.githubusercontent.com/tonlabs/debots/main/Debot.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Terminal/Terminal.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/UserInfo/UserInfo.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/SigningBoxInput/SigningBoxInput.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/SecurityCardManagement/SecurityCardManagement.sol";
+//import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Query/Query.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/QRCode/QRCode.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/NumberInput/NumberInput.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Network/Network.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Media/Media.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Hex/Hex.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/EncryptionBoxInput/EncryptionBoxInput.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/DateTimeInput/DateTimeInput.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/CountryInput/CountryInput.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Base64/Base64.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/AmountInput/AmountInput.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/AddressInput/AddressInput.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/SecurityCardManagement/SecurityCardManagement.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Sdk/Sdk.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/1d587e2899c857572d94e6cf2468ccee6e98b7ac/libraries/JsonLib.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Query/Query.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/Menu/Menu.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/1d587e2899c857572d94e6cf2468ccee6e98b7ac/libraries/JsonLib.sol";
+import "https://raw.githubusercontent.com/tonlabs/DeBot-IS-consortium/main/ConfirmInput/ConfirmInput.sol";
+
+contract TestDebot is Debot{
+
+    function start() public override {       
+        UserInfo.getSigningBox(0);
+        UserInfo.getPublicKey(0);
+        UserInfo.getAccount(0);
+        TvmCell c;
+        Terminal.printf(0, '', c);
+        SigningBoxInput.get(0, '',[tvm.pubkey()]);
+        bytes p1;
+        bytes cs;
+        uint192 sn;
+        SecurityCardManagement.createKeyForHmac(0, p1, cs, sn);
+        SecurityCardManagement.getTonWalletAppletState(0);
+        SecurityCardManagement.getSerialNumber(0);
+        SecurityCardManagement.getRecoveryData(0);
+        bytes recoveryData;
+        SecurityCardManagement.setRecoveryData(0, recoveryData);
+        bytes iv;
+        SecurityCardManagement.turnOnWallet(0, sn, p1, iv, cs);
+        SecurityCardManagement.getBlockHashes(0);         
+        uint32 s;
+        uint32 count;
+        Sdk.substring(0, '', s, count); 
+        Sdk.genRandom(0, 1);
+        uint32 boxHandle;
+        uint256 h;
+        Sdk.signHash(0, boxHandle, h);        
+        Sdk.decrypt(0, boxHandle, '');
+        Sdk.encrypt(0, boxHandle, ''); 
+        address addr;
+        Sdk.getAccountCodeHash(0, addr);
+        Sdk.getAccountType(0, addr);
+        Sdk.getBalance(0, addr);
+        uint32 limit;        
+        Query.collection(0,  
+            QueryCollection.Messages,
+            format("{\"src\":{\"eq\":\"{}\"},\"msg_type\":{\"eq\":0}}", address(this)),
+            "created_lt value dst body",
+            limit,
+            QueryOrderBy("created_lt", SortDirection.Ascending));
+            bytes prompt;
+            bytes text;
+        QRCode.draw(0, prompt, text);     
+        QRCode.read(0, prompt);
+        int256 min;
+        int256 max;
+        NumberInput.get(0, prompt, min, max);
+        bytes url;
+        bytes[] headers;
+        bytes body;
+        Network.post(0, url, headers, body);
+        Network.get(0, url, headers);
+        bytes title;
+        string description;
+        MenuItem(title, description, 0);        
+        Menu.select(title, description, [
+            MenuItem(title, description, 0)
+        ]);
+        bytes data;
+        Media.output(0, prompt, data);
+        string json;
+        //Json.deserialize(0,'');
+        string hexstr;
+        Hex.decode(0, hexstr);
+        Hex.encode(0, data);
+        EncryptionBoxInput.getSupportedAlgorithms(0);
+        uint32 handle;
+        EncryptionBoxInput.remove(0, handle);
+        bytes nonce;
+        EncryptionBoxInput.getChaCha20Box(0, prompt, nonce);
+        uint256 theirPubkey;
+        EncryptionBoxInput.getNaclBox(0, prompt, nonce, theirPubkey);
+        EncryptionBoxInput.getNaclSecretBox(0, prompt, nonce);
+        DateTimeInput.getTimeZoneOffset(0);
+        int128 defaultDatetime;
+        int128 minDatetime;
+        int128 maxDatetime;
+        uint8 minuteInterval;        
+        int16 inTimeZoneOffset;
+        DateTimeInput.getDateTime(0, "bytes prompt", defaultDatetime, minDatetime, maxDatetime, minuteInterval, inTimeZoneOffset);
+        uint32 defaultTime;
+        uint32 minTime;
+        uint32 maxTime;
+         uint8 Interval;
+        DateTimeInput.getTime(0, "bytes prompt", defaultTime, minTime, maxTime, Interval);
+        int128 defaultDate;
+        int128 minDate;
+        int128 maxDate;
+        DateTimeInput.getDate(0, "bytes prompt", defaultDate, minDate, maxDate);
+        string[] permitted;
+        string[] banned;
+        CountryInput.get(0, "bytes prompt", permitted, banned);
+        ConfirmInput.get(0, "bytes prompt");
+        Base64.decode(0, "string base64");
+        Base64.encode(0, "bytes prompt");
+        uint8 decimal;
+        uint128 min1;
+        uint128 max1;
+        AmountInput.get(0, "bytes prompt", decimal, min1, max1);
+        AddressInput.get(0, "bytes prompt");        
+    }
+    
+    function getDebotInfo() public functionID(0xDEB) override view returns(
+        string name, string version, string publisher, string key, string author,
+        address support, string hello, string language, string dabi, bytes icon
+    ) {
+        name = "Test DeBot";
+        version = "0.0.1";
+        publisher = "publisher name";
+        key = "How to use";
+        author = "Author name";
+        support = address.makeAddrStd(0, 0x000000000000000000000000000000000000000000000000000000000000);
+        hello = "Hello, i am an test DeBot.";
+        language = "en";
+        dabi = m_debotAbi.get();
+        icon = '';
+    }
+    
+    function getRequiredInterfaces() public view override returns (uint256[] interfaces) {
+        return [ Terminal.ID ];
+    }     
+}

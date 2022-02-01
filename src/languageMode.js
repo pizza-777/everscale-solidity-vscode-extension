e775669aa9949589809e62fcbc99f281c81b91e9
freeton20 <doctor4891@gmail.com>
2022-02-01 16:22:11 +0200

    upd

--- src/languageMode.js
+++ src/languageMode.js
@@ -1,9 +1,19 @@
 const vscode = require('vscode');
 
 async function setLanguageMode() {
+	if(!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.hasOwnProperty('document'))
+	{
+		return;
+	}
 	const doc = vscode.window.activeTextEditor.document;	
 	if (doc.getText().match(/ton\-solidity/g)) {
 		await vscode.languages.setTextDocumentLanguage(vscode.window.activeTextEditor.document, 'ton-solidity');	
+	}else{
+		try{
+			await vscode.languages.setTextDocumentLanguage(vscode.window.activeTextEditor.document, 'solidity');	
+		}catch(e){
+			return;
+		}	 	
 	}	
 }
 

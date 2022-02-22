7d03ac0d7fdf19ae04693b4c518bc0a2bde63a48
freeton20 <doctor4891@gmail.com>
2022-02-22 19:55:17 +0200

    filter bad links

--- src/documentLinks.js
+++ src/documentLinks.js
@@ -1,26 +1,33 @@
 const { DocumentLink, Range, Uri } = require("vscode");
 const path = require("path");
+const fs = require("fs");
 
 function documentLinks(document){
     
-    let text = document.getText();        
+    const text = document.getText();        
     const imports = text.match(/import\s+"(.*)"/gm);
     if(imports == null) return;
     const dir = path.dirname(document.uri.fsPath);
-    const links = imports.map(i=>{
+    let links = imports.map(i=>{
         const url = i.split("\"")[1];
         let newPath = url;
         if(url.match(/https?:\/\//) == null){
             newPath = path.resolve(dir, url);
-        } 
+        }      
+
         const start = text.indexOf(url);
         const end = start + url.length;
-        const positionStart = document.positionAt(start);
-        const positionEnd = document.positionAt(end);
+        let positionStart = document.positionAt(start);
+        let positionEnd = document.positionAt(end);
         const uri = Uri.parse(newPath);
+
+        if(!fs.existsSync(newPath)){
+            return;
+        };
         return new DocumentLink(new Range(positionStart, positionEnd), uri);
-    })
+    })   
     
+    links = links.filter(link => typeof link !== 'undefined');
     return links;
 }
 

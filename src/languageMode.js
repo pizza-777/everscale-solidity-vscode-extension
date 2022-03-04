3b6cdb5e6f92a8c74ff3629fbe1bd3dabd982573
freeton20 <doctor4891@gmail.com>
2022-03-04 19:20:11 +0200

    skip not sol files

--- src/languageMode.js
+++ src/languageMode.js
@@ -1,10 +1,16 @@
 const vscode = require('vscode');
+const path = require('path');
 
 async function setLanguageMode() {
 	if (!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.hasOwnProperty('document')) {
 		return;
 	}
 	const doc = vscode.window.activeTextEditor.document;
+	
+	const ext = path.extname(doc.fileName);
+	if(ext !== '.sol'){
+		return;
+	}
 
 	if (doc.getText().match(/(pragma) (ton-solidity)\s?([0-9a-zA-Z_\.\s\~|^<>=]+)?\s?(;)/gm)) {
 		if (doc.languageId !== "ton-solidity") await vscode.languages.setTextDocumentLanguage(vscode.window.activeTextEditor.document, 'ton-solidity');

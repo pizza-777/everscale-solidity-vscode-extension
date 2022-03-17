a28b159c7712c7703b5b7c565d1e1df5f48f483f
freeton20 <doctor4891@gmail.com>
2022-03-17 17:01:11 +0200

    refactoring

--- src/languageMode.js
+++ src/languageMode.js
@@ -2,21 +2,22 @@ const vscode = require('vscode');
 const path = require('path');
 
 async function setLanguageMode() {
-	if (!vscode.window.activeTextEditor || !vscode.window.activeTextEditor.hasOwnProperty('document')) {
+	const editor = vscode.window.activeTextEditor;
+	if (!editor || !editor.hasOwnProperty('document')) {
 		return;
 	}
-	const doc = vscode.window.activeTextEditor.document;
+	const doc = editor.document;
 	
 	const ext = path.extname(doc.fileName);
 	if(ext !== '.sol'){
 		return;
 	}
 
-	if (doc.getText().match(/ton-solidity|tvm\.|Tvm(cell|slice|builder)/gm)) {
-		if (doc.languageId !== "ton-solidity") await vscode.languages.setTextDocumentLanguage(vscode.window.activeTextEditor.document, 'ton-solidity');
+	if (doc.getText().match(/ton-solidity|tvm\.|Tvm(cell|slice|builder)/m)) {
+		if (doc.languageId !== "ton-solidity") await vscode.languages.setTextDocumentLanguage(doc, 'ton-solidity');
 	} else {
 		try {
-			if (doc.languageId !== "solidity" && doc.languageId !== 'Solidity') await vscode.languages.setTextDocumentLanguage(vscode.window.activeTextEditor.document, 'solidity');
+			if (doc.languageId.match(/solidity/im)) await vscode.languages.setTextDocumentLanguage(doc, 'solidity');
 		} catch (e) {
 			return;
 		}

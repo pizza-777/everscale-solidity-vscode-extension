64d0e2872fcc0eafc672c29347473a5f4ffd08ef
freeton20 <doctor4891@gmail.com>
2022-04-02 12:43:04 +0300

    fix path /filepath

--- src/documentLinks.js
+++ src/documentLinks.js
@@ -3,7 +3,6 @@ const path = require("path");
 const fs = require("fs");
 
 function documentLinks(document) {
-
     const text = document.getText();
     const imports = text.match(/import\s+["'](.*)["']/gm);
     if (imports == null) return;
@@ -11,34 +10,37 @@ function documentLinks(document) {
     let links = imports.map(i => {
         const url = i.split(/['"]/)[1];
         let newPath = url;
-        if (url.match(/https?:\/\//) == null) {
-            newPath = path.resolve(dir, url);
-        } else {
+        if (url.match(/https?:\/\//) !== null) {           
+            return;
+        }        
+
+        newPath = searchLinks(url, dir);
+        if (!newPath) {
             return;
         }
-
-        const start = text.indexOf(url);
+        
+        const start = text.indexOf(url, document);
         const end = start + url.length;
         let positionStart = document.positionAt(start);
         let positionEnd = document.positionAt(end);
-
-        if (!fs.existsSync(newPath)) {
-            newPath = searchLinks(url);
-            if (!newPath) {
-                return;
-            }
-        };
-
-        return new DocumentLink(new Range(positionStart, positionEnd), Uri.parse(newPath));
+        return new DocumentLink(new Range(positionStart, positionEnd), Uri.parse('file://' + newPath, true));
     })
 
     links = links.filter(link => typeof link !== 'undefined');
     return links;
 }
 
-//for find broxus locklift contracts in node_modules folder
-function searchLinks(url) {
-    const file_path = path.resolve(workspace.workspaceFolders[0].uri.path, "node_modules", url);
+function searchLinks(url, dir) {    
+    if (fs.existsSync(path.resolve(dir, url))) {
+        return path.resolve(dir, url);
+    }
+
+    if (fs.existsSync(path.resolve(dir, './' + url))) {
+        return path.resolve(dir, './' + url);
+    }
+
+    //find broxus locklift contracts in node_modules folder
+    const file_path = path.resolve(dir, "node_modules", url);
     if (fs.existsSync(file_path)) {
         return file_path;
     }

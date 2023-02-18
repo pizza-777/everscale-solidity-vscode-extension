42ad03c42d259a15d73d032e0ea49118946df96b
freeton20 <doctor4891@gmail.com>
2023-02-18 18:56:24 +0200

    minor

--- src/extension.js
+++ src/extension.js
@@ -205,7 +205,7 @@ function activate(context) {
 }
 
 function isDebot() {
-	if (!vscode.window.activeTextEditor.document) return false
+	if (!!vscode.window.activeTextEditor.document) return false
 	const content = vscode.window.activeTextEditor.document.getText();
 	if (content.match(/is Debot/g) == null) {
 		return false;

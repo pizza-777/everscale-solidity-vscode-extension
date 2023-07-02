9eee5e4d79719a6136b3723356057639a756a821
pizza-777 <doctor4891@gmail.com>
2023-07-02 18:08:34 +0300

    formating assemblyFunctionDefinition

---
+++ test/contracts/assembly.sol
@@ -0,0 +1,12 @@
+pragma ever-solidity >= 0.70.0;
+
+function checkOverflow(uint a, uint b) assembly pure returns (bool) {
+    //this is a comment
+                    "QADD",
+    "ISNAN",
+    "NOT",
+}
+
+function incomingValue() assembly pure returns (uint) {
+    ".blob xF82B", // it's opcode INCOMINGVALUE 
+}

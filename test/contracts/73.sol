522f058ab68eaf78cc0a0b4dba6f5c351a7a2f56
pizza-777 <doctor4891@gmail.com>
2024-03-03 10:30:36 +0200

    upd voc. 73

---
+++ test/contracts/73.sol
@@ -0,0 +1,108 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma ever-solidity 0.73.0;
+
+contract sol {
+    uint nostorage b;
+    uint constant const = 1;
+function Info() public{
+   // tvm.insertPubkey();
+    //address.makeAddrNone();
+    address.addrNone;
+    TvmCell c;
+    abi.encodeStateInit(c,c);
+    abi.encodeData({pubkey: msg.pubkey(), contr: sol, varInit: {}});
+    abi.encodeOldDataInit({pubkey: msg.pubkey(), contr: sol, varInit: {}});
+    format("{:1d}", 10) == "10";
+    abi.encodeIntMsg({
+      dest: address(0),
+      value: 0,
+      call: {Info}
+    });
+    uint31 a = 100;
+      repeat(a){}
+      int i = 1;
+i.cast(int);
+//math.muldiv(2**500, 1, 1);
+string s = 'string';
+s.substr(2, 100);
+
+
+//tvm.buildDataInit();
+abi.encodeData({
+  pubkey: 0,
+  contr: sol
+});
+TvmSlice sl;
+//s.loadStateVars();
+abi.decodeData(sol, s);
+//tvm.buildStateInit();
+abi.encodeStateInit(c,c);
+//tvm.stateInitHash();
+abi.stateInitHash(1,1,1,1);
+//tvm.codeSalt();
+abi.codeSalt(c);
+//tvm.setCodeSalt();
+abi.setCodeSalt(c,c);
+tvm.functionId(sol);
+abi.functionId(sol);
+tvm.accept();
+tvm.buildExtMsg({
+    dest: this,
+    time: 0,
+    expire: 0,
+    call: {Info},
+    // sign: ,
+    // pubkey: optional(uint256),
+    callbackId: 1,
+    onErrorId: 1
+    // stateInit: TvmCell,
+    // signBoxHandle: optional(uint32)
+});
+abi.encodeExtMsg({
+    dest: this,
+    time: 0,
+    expire: 0,
+    call: {Info},
+    // sign: ,
+    // pubkey: optional(uint256),
+    callbackId: 1,
+    onErrorId: 1
+    // stateInit: TvmCell,
+    // signBoxHandle: optional(uint32)
+});
+tvm.buildIntMsg({
+    dest: this,   
+    value: 0,   
+    call: {Info}
+    // sign: ,
+    // pubkey: optional(uint256),
+    
+    // stateInit: TvmCell,
+    // signBoxHandle: optional(uint32)
+});
+abi.encodeIntMsg({ dest: this,   
+    value: 0,   
+    call: {Info}});
+tvm.encodeBody(Info);
+abi.encodeBody(Info);
+//s.loadFunctionParams(Info);
+//?????abi.decodeFunctionParams(Info);
+//s.loadTons();
+TvmBuilder  b;
+b.storeTons(1);
+
+//sl.load();
+uint l = 1;
+b.store(l); 
+require(true, 'text');
+c.exoticToSlice();
+c.loadExoticCell();
+c.loadExoticCellQ();
+b.toExoticCell();
+
+b.store(l);
+sl.skip(10, 2);
+sl.skip(10, 2);
+    
+}
+}
